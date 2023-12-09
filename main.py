fad9c366a4996a7df97b5f0eecba4d287d0f3dd4
Aleksandrs Barbins <bbarbins@gmail.com>
2023-12-09 19:47:22 +0200

    added middleware

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import base64
 import uvicorn as uvicorn
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from encryption import encrypt, decrypt
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3000",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 class RequestBody(BaseModel):
@@ -29,4 +38,5 @@ async def root(body: RequestBody):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)  # Dont run this
+    # Run this in terminal: uvicorn main:app --reload
