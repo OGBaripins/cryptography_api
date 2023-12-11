bb9112ba178ca0f9ce520dfd081ba63122a395bc
Aleksandrs Barbins <bbarbins@gmail.com>
2023-12-11 20:57:12 +0200

    small changes

--- encryption.py
+++ encryption.py
@@ -93,7 +93,6 @@ def mix_columns(s):
 
 
 def mix_single_column(a):
-    # see Sec 4.1.2 in The Design of Rijndael
     t = a[0] ^ a[1] ^ a[2] ^ a[3]
     u = a[0]
     a[0] ^= t ^ xtime(a[0] ^ a[1])
@@ -103,7 +102,6 @@ def mix_single_column(a):
 
 
 def inv_mix_columns(s):
-    # see Sec 4.1.3 in The Design of Rijndael
     for i in range(4):
         u = xtime(xtime(s[i][0] ^ s[i][2]))
         v = xtime(xtime(s[i][1] ^ s[i][3]))
